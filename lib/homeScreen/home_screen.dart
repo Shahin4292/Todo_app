dc60ece05eb97a1c34b54c1db35d52eb10d91c0d
Shahin4292 <shahinsarker878@gmail.com>
2024-05-30 19:41:24 +0600

    home screen done

--- lib/homeScreen/home_screen.dart
+++ lib/homeScreen/home_screen.dart
@@ -15,18 +15,39 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Todo"),
         centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () { 
-        
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        showModalBottomSheet(context: context, builder: (context) {
+          return  Column(
+            children: [
+              const Text("Add"),
+              TextFormField(
+                decoration: InputDecoration(
+
+                ),
+              )
+            ],
+          )
+        });
       },
         child: const Icon(Icons.add),
       ),
-      body: ListView.builder(
-          itemBuilder: (context, index) => (
-          const ListTile(
-            title: Text("I have to do my Homework for office"),
-            leading: Text("pending"),
-          )
-          ),
+      body: ListView.separated(
+        itemCount: 10,
+        itemBuilder: (context, index) =>
+        (
+            ListTile(
+              title: const Text("I have to do my Homework for office"),
+              subtitle: const Text("4-04-20"),
+              trailing: const Text("pending"),
+              leading: CircleAvatar(
+                child: Text('${index + 1}'),
+              ),
+            )
+        ), separatorBuilder: (BuildContext context, int index) {
+        return const Divider(
+          height: 4,
+        );
+      },
       ),
     );
   }
