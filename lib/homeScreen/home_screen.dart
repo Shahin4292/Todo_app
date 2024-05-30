adbee35358ee00ef3048c7672664b914873380a3
Shahin4292 <shahinsarker878@gmail.com>
2024-05-30 20:57:18 +0600

    floatingActionButton complete

--- lib/homeScreen/home_screen.dart
+++ lib/homeScreen/home_screen.dart
@@ -15,39 +15,60 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Todo"),
         centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        showModalBottomSheet(context: context, builder: (context) {
-          return  Column(
-            children: [
-              const Text("Add"),
-              TextFormField(
-                decoration: InputDecoration(
-
-                ),
-              )
-            ],
-          )
-        });
-      },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const Text("Add todo"),
+                      TextFormField(
+                        maxLines: 4,
+                        decoration: const InputDecoration(
+                          // fillColor: Colors.grey,
+                          // filled: true,
+                          hintText: "Add todo work",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(6),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20,),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6),),
+                          fixedSize: Size(MediaQuery.sizeOf(context).width, 30)
+                        ),
+                        onPressed: () {},
+                        child: const Text("Add"),
+                      ),
+                    ],
+                  ),
+                );
+              });
+        },
         child: const Icon(Icons.add),
       ),
       body: ListView.separated(
         itemCount: 10,
-        itemBuilder: (context, index) =>
-        (
-            ListTile(
-              title: const Text("I have to do my Homework for office"),
-              subtitle: const Text("4-04-20"),
-              trailing: const Text("pending"),
-              leading: CircleAvatar(
-                child: Text('${index + 1}'),
-              ),
-            )
-        ), separatorBuilder: (BuildContext context, int index) {
-        return const Divider(
-          height: 4,
-        );
-      },
+        itemBuilder: (context, index) => (ListTile(
+          title: const Text("I have to do my Homework for office"),
+          subtitle: const Text("4-04-20"),
+          trailing: const Text("pending"),
+          leading: CircleAvatar(
+            child: Text('${index + 1}'),
+          ),
+        )),
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider(
+            height: 4,
+          );
+        },
       ),
     );
   }
